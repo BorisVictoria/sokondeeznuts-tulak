fbe32bae0dd9dea21bcc092e8abdd2f81c2a3690
Boris Victoria <boris.v.victoria@gmail.com>
2023-10-14 04:08:48 +0800

    Removed position of boxes in state

--- src/solver/State.java
+++ src/solver/State.java
@@ -3,20 +3,11 @@ package solver;
 public class State
 {
     private Pos player;
-    private Pos[] boxes;
-
     private char[][] itemsData;
-
     private long hash;
-
     public Pos getPlayer() {
         return player;
     }
-
-    public Pos[] getBoxes() {
-        return boxes;
-    }
-
     public char[][] getItemsData() {
         return itemsData;
     }
